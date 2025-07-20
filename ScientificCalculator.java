d74d1326ea4f46d98f2618590a231eabc2834eb5
danalawati <dana.allawaty@gmail.com>
2025-07-20 14:28:10 +0400

    feat: Initial project setup for ScientificCalculator

---
+++ ScientificCalculator.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ScientificCalculator {
+
+    public static void main(String[] args) {
+        // Main method placeholder
+    }
+
+    public static void displayMenu() {
+        // Menu display placeholder
+    }
+}
