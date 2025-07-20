cb16ddd4fd5bed438d8763d31739d1d14bbfad4c
danalawati <dana.allawaty@gmail.com>
2025-07-20 16:23:35 +0400

    feat: Add division functionality with zero division error handling

--- ScientificCalculator/src/ScientificCalculator.java
+++ ScientificCalculator/src/ScientificCalculator.java
@@ -6,7 +6,8 @@ public class ScientificCalculator {
         Scanner scanner = new Scanner(System.in);
         // performAddition(scanner);
         //performSubtraction(scanner);
-        performMultiplication(scanner);
+        //performMultiplication(scanner);
+        performDivision(scanner);
     }
     public static void performAddition(Scanner scanner) {
         System.out.print("Enter the first number: ");
@@ -38,6 +39,20 @@ public class ScientificCalculator {
         double num2 = scanner.nextDouble();
 
         double result = num1 * num2;
+        System.out.println("Result: " + result);
+    }
+    public static void performDivision(Scanner scanner) {
+        System.out.print("Enter the numerator: ");
+        double numerator = scanner.nextDouble();
+
+        System.out.print("Enter the denominator: ");
+        double denominator = scanner.nextDouble();
+
+        if (denominator == 0) {
+            System.out.println("Error: Division by zero is not allowed.");
+            return;
+        }
+        double result = numerator / denominator;
         System.out.println("Result: " + result);
     }
     public static void displayMenu() {
