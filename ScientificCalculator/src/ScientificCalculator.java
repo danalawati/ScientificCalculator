f6a8587815495b82c6daddc3a758d093b8f5cf09
danalawati <dana.allawaty@gmail.com>
2025-07-20 16:20:38 +0400

    feat: Add performMultiplication method

--- ScientificCalculator/src/ScientificCalculator.java
+++ ScientificCalculator/src/ScientificCalculator.java
@@ -5,7 +5,8 @@ public class ScientificCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // performAddition(scanner);
-        performSubtraction(scanner);
+        //performSubtraction(scanner);
+        performMultiplication(scanner);
     }
     public static void performAddition(Scanner scanner) {
         System.out.print("Enter the first number: ");
@@ -26,6 +27,17 @@ public class ScientificCalculator {
         double num2 = scanner.nextDouble();
 
         double result = num1 - num2;
+        System.out.println("Result: " + result);
+    }
+
+    public static void performMultiplication(Scanner scanner){
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        double result = num1 * num2;
         System.out.println("Result: " + result);
     }
     public static void displayMenu() {
