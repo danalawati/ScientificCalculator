3bd21a62be37017afeec2e77700263625be8ba9a
danalawati <dana.allawaty@gmail.com>
2025-07-20 16:17:00 +0400

    feat: Add performSubtraction method

--- ScientificCalculator/src/ScientificCalculator.java
+++ ScientificCalculator/src/ScientificCalculator.java
@@ -4,7 +4,8 @@ public class ScientificCalculator {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        performAddition(scanner); // temporary call for testing
+        // performAddition(scanner);
+        performSubtraction(scanner);
     }
     public static void performAddition(Scanner scanner) {
         System.out.print("Enter the first number: ");
@@ -14,6 +15,17 @@ public class ScientificCalculator {
         double num2 = scanner.nextDouble();
 
         double result = num1 + num2;
+        System.out.println("Result: " + result);
+    }
+
+    public static void performSubtraction(Scanner scanner){
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        double result = num1 - num2;
         System.out.println("Result: " + result);
     }
     public static void displayMenu() {
